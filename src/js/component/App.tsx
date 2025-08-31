f62641c4d3089315380c38f913d4f71a8dd4dc80
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-31 04:30:53 +0000

    feat: init physics [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -2,20 +2,39 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import {ContactShadows, OrbitControls} from '@react-three/drei'
+import {OrbitControls, SoftShadows} from '@react-three/drei'
 import {Canvas} from '@react-three/fiber'
+import {Physics, RigidBody} from '@react-three/rapier'
 
 export const App = () => {
   return (
-    <Canvas camera={{fov: 35, position: [4, 1, 8]}}>
+    <Canvas camera={{fov: 35, position: [4, 1, 8]}} shadows={true}>
+      <SoftShadows />
+
       <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
 
-      <ContactShadows opacity={0.5} blur={3} position={[0, -0.5, 0]} />
+      <ambientLight intensity={2} />
+      <directionalLight intensity={5} position={[1, 1, 1]} castShadow={true} />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Physics>
+        <RigidBody>
+          <mesh position={[0, 3, 0]} castShadow={true}>
+            <boxGeometry />
+            <meshStandardMaterial color="lime" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody type="fixed">
+          <mesh
+            scale={[10, 0.1, 10]}
+            position={[0, -0.05, 0]}
+            receiveShadow={true}
+          >
+            <boxGeometry />
+            <meshStandardMaterial color="hotpink" />
+          </mesh>
+        </RigidBody>
+      </Physics>
     </Canvas>
   )
 }
